b6abfce9ed5f2b137b4d76347ac805296dc3f75f
FernandoAlv24 <100179088+FernandoAlv24@users.noreply.github.com>
2023-05-03 22:18:53 -0500

    Primeros pasos angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Hola-Mundo';
+listEstudiantes: any[] =[
+  {nombre:'Fernando Alvarez', grado:'9'},
+  {nombre:'Camila Velez', grado:'8'},
+  {nombre:'Bella Betancourt', grado:'7'}
+
+]
+mostrar=true;
+toogle(): void{
+  this.mostrar =! this.mostrar
+}
 }
